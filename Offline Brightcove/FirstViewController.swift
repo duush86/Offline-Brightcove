b944543d50bdd1069f77eac519b8ddf32ba88e97
Antonio <j.antonio.oro@gmail.com>
2019-10-05 15:19:59 -0500

    Initial Commit

---
+++ Offline Brightcove/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Offline Brightcove
+//
+//  Created by Antonio Orozco on 10/5/19.
+//  Copyright © 2019 Antonio Orozco. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Offline Brightcove/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Offline Brightcove
+//
+//  Created by Antonio Orozco on 10/5/19.
+//  Copyright © 2019 Antonio Orozco. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
